e3e29c18db6e9e12a6290074005747dde943e650
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2025-05-14 19:32:24 +0300

    redirect bug fixed

--- src/middleware/get-session-data.middleware.ts
+++ src/middleware/get-session-data.middleware.ts
@@ -12,9 +12,8 @@ import { defineMiddleware } from "astro:middleware";
  */
 export const getUserSessionDataMiddleware = defineMiddleware(async (c, next) => {
   const session = await c.session?.get("user");
-  if (!session?.email) {
-    return c.redirect("/auth/sign-in");
+  if (session?.email) {
+    c.locals.user = { userId: session.userId, email: session.email };
   }
-  c.locals.user = { userId: session.userId, email: session.email };
   return await next();
 });
